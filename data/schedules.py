52ee2ae233900d708c0269fabfc79b07dce443bf
Francesco <38871879+Eyon42@users.noreply.github.com>
2021-11-19 21:05:57 -0300

    Update schedule for event

--- data/schedules.py
+++ data/schedules.py
@@ -2,8 +2,170 @@ from datetime import datetime
 
 scheduled_messages = [
     {
-        "datetime": datetime(2021,11,11,1,15),
-        "message": "testing",
+        "datetime": datetime(2021,11,20,9,0),
+        "message": "Buenos Días! Comienza el primer día del Evento 🚀",
+        "channelID": 900562222223327273
+    },
+    {
+        "datetime": datetime(2021,11,20,10,0),
+        "message": "Charla de Bienvenida en Aula Magna. Piso 2",
+        "channelID": 900562222223327273
+    },
+    {
+        "datetime": datetime(2021,11,20,10,30),
+        "message": "Dinámica: Tesoro escondido 🔑",
+        "channelID": 900562222223327273
+    },
+    {
+        "datetime": datetime(2021,11,20,11,0),
+        "message": "Charla: Motivar y inspirar a seguir nuestros propositos. Florencia Benedicto 🗣",
+        "channelID": 900562222223327273
+    },
+    {
+        "datetime": datetime(2021,11,20,11,30),
+        "message": "Charla: Descripciones de soluciones y Diseño de popuesta de valor. Fabio Totera 🗣",
+        "channelID": 900562222223327273
+    },
+    {
+        "datetime": datetime(2021,11,20,13,30),
+        "message": "Break: Almuerzo y Descanso 🍗 🍅",
+        "channelID": 900562222223327273
+    },
+    {
+        "datetime": datetime(2021,11,20,14,0),
+        "message": "Charla: Entender tu nicho de mercado. Zeke Novarino 🗣",
+        "channelID": 900562222223327273
+    },
+    {
+        "datetime": datetime(2021,11,20,14,30),
+        "message": "Charla: Entender las Métricas de tu negocio. Marcos Bruno 🗣",
+        "channelID": 900562222223327273
+    },
+    {
+        "datetime": datetime(2021,11,20,15,0),
+        "message": "Charla: No tan financiera, sino más económica, Germán Gimenez 🗣",
+        "channelID": 900562222223327273
+    },
+    {
+        "datetime": datetime(2021,11,20,15,30),
+        "message": "Charla: Dar un abanico de oportunidades de financiamiento. Francisco Buchara 🗣",
+        "channelID": 900562222223327273
+    },
+    {
+        "datetime": datetime(2021,11,20,16,0),
+        "message": "Charla: Una visión desde lo comercial y económico. Omar Nievas 🗣",
+        "channelID": 900562222223327273
+    },
+    {
+        "datetime": datetime(2021,11,20,17,30),
+        "message": "Merienda 🥐",
+        "channelID": 900562222223327273
+    },
+    {
+        "datetime": datetime(2021,11,20,18,0),
+        "message": "Charla: Nuevas herramientas tecnológicas (Economía Crypto). Juan Pablo Brondo🗣",
+        "channelID": 90056222222332727
+    },
+    {
+        "datetime": datetime(2021,11,20,18,30),
+        "message": "Charla: Interdisciplina y empuje para alcanzar el propósito de mejorar la salud de la gente. Juan Bustamante 🗣",
+        "channelID": 900562222223327273
+    },
+    {
+        "datetime": datetime(2021,11,20,19,0),
+        "message": "Charla: Nuevas herramientas tecnológicas (Blockchain). Arturo Grande 🗣",
+        "channelID": 900562222223327273
+    },
+    {
+        "datetime": datetime(2021,11,20,19,30),
+        "message": "Charla: Nuevas herramientas tecnológicas (Biotecnología). Paz Alvarez 🗣",
+        "channelID": 900562222223327273
+    },
+    {
+        "datetime": datetime(2021,11,20,20,0),
+        "message": "Charla: Avances en las tranformación digital en el Marketin. Ismael Briasco 🗣",
+        "channelID": 900562222223327273
+    },
+    # Dia 2
+    {
+        "datetime": datetime(2021,11,21,10,0),
+        "message": "Buenos Días! Comienza el segundo día del Evento 🚀",
+        "channelID": 900562222223327273
+    },
+    {
+        "datetime": datetime(2021,11,21,11,0),
+        "message": "¿Cómo armar un MVP? 🗣",
+        "channelID": 900562222223327273
+    },
+    {
+        "datetime": datetime(2021,11,21,12,0),
+        "message": "Presentación de herramientas para crear MVPs 🛠",
+        "channelID": 900562222223327273
+    },
+    {
+        "datetime": datetime(2021,11,21,16,30),
+        "message": "Dinamicas en equipo con Global Shapers",
+        "channelID": 900562222223327273
+    },
+    {
+        "datetime": datetime(2021,11,21,17,0),
+        "message": "Charla: Como vender una idea. Fabio Totera 🗣",
+        "channelID": 900562222223327273
+    },
+    {
+        "datetime": datetime(2021,11,21,18,0),
+        "message": "Charla: Nomade digital: Maneras alternativas de emprender un negocio. Fede Bongiorno 🗣",
+        "channelID": 900562222223327273
+    },
+    {
+        "datetime": datetime(2021,11,21,19,0),
+        "message": "Dinamica: Yoga con método DeRose",
+        "channelID": 900562222223327273
+    },
+    {
+        "datetime": datetime(2021,11,21,19,30),
+        "message": "Cierre del día y aprendizajes",
+        "channelID": 900562222223327273
+    },
+    # Dia 3
+    {
+        "datetime": datetime(2021,11,22,13,0),
+        "message": "Buenos Días! Comienza el último día del Evento 🚀",
+        "channelID": 900562222223327273
+    },
+    {
+        "datetime": datetime(2021,11,22,13,30),
+        "message": "Preguntas sobre como dar el pitch",
+        "channelID": 900562222223327273
+    },
+    {
+        "datetime": datetime(2021,11,22,14,0),
+        "message": "Taller de teatro: Herramientas de improvisación",
+        "channelID": 900562222223327273
+    },
+    {
+        "datetime": datetime(2021,11,22,16,0),
+        "message": "Charla Inspiracional 🗣",
+        "channelID": 90056222222332727
+    },
+    {
+        "datetime": datetime(2021,11,22,15,30),
+        "message": "Charla: Interdisciplina y empuje para alcanzar el propósito de mejorar la salud de la gente. Juan Bustamante 🗣",
+        "channelID": 900562222223327273
+    },
+    {
+        "datetime": datetime(2021,11,22,16,45),
+        "message": "Pitcheo y competencia 🚀",
+        "channelID": 900562222223327273
+    },
+    {
+        "datetime": datetime(2021,11,22,18,0),
+        "message": "Entrega de premios 🏆",
+        "channelID": 900562222223327273
+    },
+    {
+        "datetime": datetime(2021,11,22,18,30),
+        "message": "Continuación de los proyectos 😁🚀",
         "channelID": 900562222223327273
     }
 ]
